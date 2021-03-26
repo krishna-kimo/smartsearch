d55c0f8ee4087b44e440e4ea049af03e7eec2b40
Krishna Nallamilli <krishna@kimo.ai>
2021-03-26 23:10:57 +0530

    Bugs resolved

--- search_api/app/main.py
+++ search_api/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-
+import requests
+import json
 
 class Item(BaseModel):
     term: str
@@ -8,9 +9,48 @@ class Item(BaseModel):
 
 app = FastAPI()
 
-def make_retriever_req(query):
-    pass
+def make_ret_search(entities):
+    url = "http://34.107.125.154:8081/postretriever/"
+    payload = {}
+    payload['input'] = {}
+    payload['input']['queryTerms'] = [entities['query'].lower()]
+    payload['input']['numOfRetrievedRecords'] = 10
+    payload['input']['allowedContentTypes'] = [entities['content_type'].lower()]
+
+    res = requests.post(url=url, data=json.dumps(payload))
+
+    return res.json()
+    #return json.dumps(payload)
+def parse_entities(jsonObj):
+    req_entities = {}
+    for entity in jsonObj['entities']:
+        if entity['entity'] == "content_type":
+            req_entities['content_type'] = entity['value']
+        elif entity['entity'] == "search_term":
+            req_entities['query'] = entity['value']
+        ## Add time later
+
+    return req_entities
+
+def fetch_entities(query):
+    url = "http://rasa:5005/model/parse"
+    payload = {}
+    payload['text'] = "Articles for Machine learning for 5 min"
+
+    res = requests.post(url=url, data=json.dumps(payload)) 
+    entities = parse_entities(res.json())
+
+    res = make_ret_search(entities)
+
+    return res
+    #return "Fuck you"
+
+
+@app.post("/")
+async def greet():
+    return "hello"
+
 
 @app.post("/search/")
 async def search_query(query: Item):
-    return query.term
+    return fetch_entities(query.term)
